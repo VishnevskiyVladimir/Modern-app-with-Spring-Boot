581e48eea1c37595bab08f04ad7d6baceeff269a
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2021-05-25 16:03:39 +0300

    add lombok

--- src/main/java/com/example/springboot/lesson/config/ApplicationConfiguration.java
+++ src/main/java/com/example/springboot/lesson/config/ApplicationConfiguration.java
@@ -1,21 +1,18 @@
 package com.example.springboot.lesson.config;
 
 import com.example.springboot.lesson.conditional.FirstConditional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
 
+@Slf4j
 @Conditional(FirstConditional.class)
-@Profile("dev")
 @Configuration
 public class ApplicationConfiguration {
-    private static final Logger log = LoggerFactory.getLogger(ApplicationConfiguration.class);
     @PostConstruct
-    public void init(){
+    public void init() {
         log.warn("our configuration is loaded");
     }
 }
