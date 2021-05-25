0f61a06fab6efd1f517810a3d78bb54c8f296709
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2021-05-25 15:39:46 +0300

    conditional and autoconfigure

---
+++ src/main/java/com/example/springboot/lesson/config/ApplicationConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.springboot.lesson.config;
+
+import com.example.springboot.lesson.conditional.FirstConditional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
+
+@Conditional(FirstConditional.class)
+@Profile("dev")
+@Configuration
+public class ApplicationConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(ApplicationConfiguration.class);
+    @PostConstruct
+    public void init(){
+        log.warn("our configuration is loaded");
+    }
+}
