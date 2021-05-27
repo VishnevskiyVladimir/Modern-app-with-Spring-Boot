ec90f4601190eb2d7580aa8e004f5c84bbe16a34
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2021-05-27 09:59:31 +0300

    data jpa starter adding entities and repositories

---
+++ src/main/java/com/example/springboot/lesson/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.springboot.lesson.repository;
+
+
+import com.example.springboot.lesson.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer> {
+}
